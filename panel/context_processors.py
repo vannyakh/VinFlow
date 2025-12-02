b454926d1f51910570d264d642206da69d25a660
“vannyakh” <vannakkm33@gmail.com>
2025-12-02 22:54:35 +0700

    Remove obsolete files and templates, including .gitignore, environment configurations, Makefile, manage.py, and various panel views and templates, to streamline the project structure and eliminate unused code.

--- panel/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.utils import translation
-
-def language_processor(request):
-    return {
-        'current_language': translation.get_language(),
-        'is_khmer': translation.get_language() == 'km',
-    }
-
--- panel/templatetags/__init__.py
+++
@@ -1,2 +0,0 @@
-# Template tags for panel app
-
